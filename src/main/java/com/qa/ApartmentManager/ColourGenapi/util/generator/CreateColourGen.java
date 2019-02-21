b594ac338cfbb8afc945bc072805a27f4912ab0b
Husnan Sultan <husnansultan@academytrainee.com>
2019-02-21 14:56:03 +0000

    updted colour gen
    updted colour gen

--- src/main/java/com/qa/ApartmentManager/ColourGenapi/util/generator/CreateColourGen.java
+++ src/main/java/com/qa/ApartmentManager/ColourGenapi/util/generator/CreateColourGen.java
@@ -1,39 +1,37 @@
 package com.qa.ApartmentManager.ColourGenapi.util.generator;
+
 import java.awt.Color;
 import java.util.Random;
 
 public class CreateColourGen {
-	
+
 	public String getCreateColourGen() {
-	
+
 		Random rand = new Random();
-		
-		// Java 'Colour' class takes 3 floats, from 0 to 1.
-		float r = rand.nextFloat();
-		float g = rand.nextFloat();
-		float b = rand.nextFloat();
-		
-		Color randomColor = new Color(r, g, b);
-		
-		String hex = toHex(randomColor); 
-		
-	return hex;
-	
+
+		float red = rand.nextFloat();
+		float green = rand.nextFloat();
+		float blue = rand.nextFloat();
+
+		Color randomColor = new Color(red, green, blue);
+
+		return toHex(randomColor);
+
 	}
-	
+
 	public static String toHex(Color randomColor) {
 		if ((randomColor.getRed() & randomColor.getGreen() & randomColor.getBlue()) == 0) {
 			randomColor.brighter();
-			return "#" + toBrowserHexValue(randomColor.getRed()) + toBrowserHexValue(randomColor.getGreen()) + toBrowserHexValue(randomColor.getGreen());
 		}
-	    return "#" + toBrowserHexValue(randomColor.getRed()) + toBrowserHexValue(randomColor.getGreen()) + toBrowserHexValue(randomColor.getGreen());
-	  }
+		return "#" + toBrowserHexValue(randomColor.getRed()) + toBrowserHexValue(randomColor.getGreen())
+				+ toBrowserHexValue(randomColor.getGreen());
+	}
 
-	  private static String toBrowserHexValue(int number) {
-	    StringBuilder builder = new StringBuilder(Integer.toHexString(number & 0xff));
-	    while (builder.length() < 2) {
-	      builder.append("0");
-	    }
-	    return builder.toString().toUpperCase();
-	  }
+	private static String toBrowserHexValue(int number) {
+		StringBuilder builder = new StringBuilder(Integer.toHexString(number & 0xff));
+		for (int i = builder.length(); i < 2; i++) {
+			builder.append("0");
+		}
+		return builder.toString().toUpperCase();
+	}
 }
